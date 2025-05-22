efbad1a0e912935f72c5483de7a0cfe02d358150
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-22 13:52:49 +0200

    Ejercicio24

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,24 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Smith");
+        queue.add("Montessori");
+        queue.add("Peralta");
+        queue.add("House");
+        return queue;
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
 
+    public static void printAndEmptyQueue(Queue<String> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> queue = createQueue();
+        System.out.println("Queue elements:");
+        printAndEmptyQueue(queue);
     }
 
 }
